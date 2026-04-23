d919e82b56f8ad51f14b8e0f722bd727ff98aa01
KatieFleming1925 <100570076@alumnos.uc3m.es>
2026-04-23 15:44:22 +0200

    deleted assign10 to restart

--- assignment10/analyses/assignment10.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(ggplot2)
-library(broom)
-
-# Loading the dataset
-raw=haven::read_dta("https://github.com/franvillamil/AQM2/raw/refs/heads/master/datasets/other/corruption.dta")
-
-# Running one regression model
-
